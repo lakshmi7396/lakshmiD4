04d97c091f9e2516a625a215da7302dd7c84c3f6
lakshmi7396 <subhalakshmi7396@gmail.com>
2025-03-29 14:45:48 +0530

    Add files via upload

---
+++ PRG2.py
@@ -0,0 +1,21 @@
+def_calculate_distance(x1,y1,x2,y2)
+    """
+    calculate the distance between two points(x1,y1) and (x2,y2).
+   :param x1:x-coordinate of the first point(float or int).
+   :param y1:y-coordinate of the first point(float or int).
+   :param x2:x-coordinate of the second point(float or int).
+   :param y2:y-coordinaye of the second point(float or int).
+   :return: The distance between the two point(float).
+   """
+   distance=math.sqrt((x2-x1)**2+(y2-y1)**2)
+   return distance
+
+#input coordinates
+x1=float(input("enter x-coordinates of the first point:"))
+y1=float(input("enter y-coordinates of the first point:"))
+x2=float(input("enter x-coordinates of the second point:"))
+y2=float(input("enter y-coordinates of the second point:"))
+#calculate distance
+disance=calculat_distance(x1,y1,x2,y2)
+#output the result
+print(f"The distance between the points({x1},{y1}) and ({x2},{y2}) is {distance:.2f}") 
